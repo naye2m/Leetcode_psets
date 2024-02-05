62940db2ab5ffc16ae19d76c8ee8cc1a49b70933
naye2m <126262005+naye2m@users.noreply.github.com>
2024-02-05 16:02:40 +0600

    Time: 9 ms (65.08%), Space: 6.7 MB (95.04%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.c
@@ -0,0 +1,19 @@
+
+// Solved in 6min 40sec
+int firstUniqChar(char* s) {
+    char* cs = calloc(sizeof(char), 26);
+
+    int cl = strlen(s);
+    // finding all single chars
+    for (int i = 0; i < cl; i++) {
+        int cp = s[i] - 'a';
+        if (cs[cp] < 2)
+            cs[cp]++;
+    }
+    for (int i = 0; i < cl; i++) {
+        int cp = s[i] - 'a';
+        if (cs[cp] == 1)
+            return i;
+    }
+    return -1;
+}
